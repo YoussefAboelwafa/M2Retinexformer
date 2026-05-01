5405fcec053bcabb1c17672d128133ad8280b5bd
youssefaboelwafa <youssef.hossam49@gmail.com>
2026-05-02 02:05:40 +0300

    Fix version

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Wed Jan  7 22:45:53 2026
-__version__ = '1.2.0+451b3f9'
+# TIME: Sat May  2 02:02:05 2026
+__version__ = '1.2.0+2dea81a'
 short_version = '1.2.0'
 version_info = (1, 2, 0)
