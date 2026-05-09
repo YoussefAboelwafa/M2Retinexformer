2aad9d0baf0addf8e8b1b5b446e57dff4991f7e3
youssefaboelwafa <youssef.hossam49@gmail.com>
2026-05-09 06:35:51 +0300

    Remove unused dependencies

--- requirements.sh
+++ requirements.sh
@@ -6,6 +6,6 @@ pip install torch torchvision torchaudio --index-url https://download.pytorch.or
 
 pip install matplotlib scikit-learn scikit-image opencv-python yacs joblib natsort h5py tqdm tensorboard
 
-pip install einops gdown addict future lmdb numpy pyyaml requests scipy yapf lpips thop timm nvitop
+pip install einops gdown addict future lmdb numpy pyyaml requests scipy yapf lpips thop timm
 
 python setup.py develop --no_cuda_ext
